c6f2c731ccab072f1e8a34f988956fd39843d787
Deep Hansda <deephansda921@gmail.com>
2023-03-27 12:08:11 +0530

    backend working

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,6 +1,81 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 contract CrowdFunding {
-    constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public numOfCampaigns = 0;
+
+    // address public sender = msg.sender;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numOfCampaigns];
+
+        require(
+            campaign.deadline < block.timestamp,
+            "The deadline should be a date in the future"
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.image = _image;
+
+        numOfCampaigns++;
+        return numOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(campaign.owner);
+        campaign.donations.push(amount);
+
+        (bool success, ) = payable(campaign.owner).call{value: amount}("");
+
+        if (success) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id)
+        public
+        view
+        returns (address[] memory, uint256[] memory)
+    {
+        Campaign storage campaign = campaigns[_id];
+        return (campaign.donators, campaign.donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaingns = new Campaign[](numOfCampaigns);
+
+        for (uint256 i = 0; i < numOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaingns[i] = item;
+        }
+
+        return allCampaingns;
+    }
 }
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,7 +1,19 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+const privateKey = process.env.PRIVATE_KEY;
+const rpcUrl = process.env.RPC_URL;
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "sepolia",
+    networks: {
+      sepolia: {
+        url: rpcUrl,
+        accounts: [privateKey],
+        chainId: 11155111,
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
